5b9e7289dce12c31052c5fe80dcc42f7e063ec8c
Nowshad247 <lewsedmond@gmail.com>
2023-11-12 23:10:12 +0600

    hasky test 4

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,17 @@
-import express, { Application, Request, Response } from "express";
-import config from "./config";
-import cors from "cors";
+import express, { Application, Request, Response } from 'express'
+import cors from 'cors'
 
-const app: Application = express();
-const port = config.port;
+const app: Application = express()
 
-app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Api work Succesfully ");
-});
+app.get('/', (req: Request, res: Response) => {
+  res.send('Perfect app is working ')
+})
+app.get('/Home', (req: Request, res: Response) => {
+  res.send('Perfect app is working ')
+})
 
-export default app;
+export default app
